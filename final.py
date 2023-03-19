24ec62cd2ad9172191626ad059ce723031508ebf
Rakece <111010001+plouisn123@users.noreply.github.com>
2023-03-19 20:40:02 +0100

    Update final.py

--- final.py
+++ final.py
@@ -152,30 +152,10 @@ while True:
                 print('Quantity',quantity)
                 
                 #ordres
-                '''
+                
                 exchange.create_order(symbol=symbol, type='Limit', side=BorS, amount=quantity, price=PE)            #Entry
                 exchange.create_order(symbol=symbol, type='Limit', side=close, amount=quantity, price=TPs[2])       #TP
                 if BorS == 'Buy':
                     exchange.create_limit_order(symbol=symbol, side=close, amount=quantity, price=SL, params={'stopLossPrice': SL}) #SL close long
                 else:
                     exchange.create_limit_buy_order(symbol=symbol, amount=quantity, price=SL, params = {'stopPrice': SL}) #SL 
-                '''
-                 order_params = {
-                    'symbol': symbol,
-                    'type': 'limit',
-                    'side': BorS,
-                    'amount': quantity,
-                    'price': PE,
-                    'params': {
-                        'stopLoss': {
-                            'type': 'limit',
-                            'price': SL,
-                            'reduceOnly': True
-                        },
-                        'takeProfit': {
-                            'type': 'limit',
-                            'price': TPs[2]
-                        }
-                    }
-                }
-                exchange.create_order(**order_params)
