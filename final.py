28b2f07bedaa4648c89e00a12eb712ffc3190e2a
Rakece <111010001+plouisn123@users.noreply.github.com>
2023-03-20 21:46:03 +0100

    Update final.py

--- final.py
+++ final.py
@@ -36,10 +36,10 @@ while True:
                 #print(type(close))
                 
             #Paire    
-            start = text[1]
+            start = text.find("")
             end = text.find("(", start)
             if start != -1 and end != -1:
-                crypto = text[start:end-1]
+                crypto = text[start+1:end-1]
                 symbol = crypto+'USDT'
                 print('Paire:',symbol)
                 #print(type(symbol))
